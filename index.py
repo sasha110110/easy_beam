352817e5a7f3625ff4f057cf4b913e41519cf4fe
Yummie <58485350+sasha110110@users.noreply.github.com>
2023-02-03 10:09:28 +0300

    Create index.py

---
+++ index.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+from flask_wtf import FlaskForm
+from wtforms import SubmitField, TextAreaField, SelectField, RadioField, StringField
+from wtforms.validators import DataRequired,EqualTo, ValidationError, InputRequired
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'kek'
+
+@app.route('/', methods=['GET', 'POST'])
+def display():
+    return "Check success"
+    
+  

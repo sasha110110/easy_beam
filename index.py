0950eb64fe429a4484438692fe3744ed3f5ac262
Yummie <58485350+sasha110110@users.noreply.github.com>
2023-03-09 12:51:32 +0300

    Update index.py

--- index.py
+++ index.py
@@ -119,8 +119,8 @@ def check_payment_webhook():
 
 @app.route('/webhook2')
 def webhook2():
-    #payment_info=session.get("payment_info", None)
-    #payment_info=request.get_json()
+    payment_info=session.get("payment_info", None)
+    payment_info=request.get_json()
     #flash("payment_info")
     payment_info = json.loads(request.data, strict=False) # strict = False allow for escaped char
     

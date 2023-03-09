0f02bc7556ae4e35ec9a4d4b3559d2741ab53242
Yummie <58485350+sasha110110@users.noreply.github.com>
2023-03-09 12:43:49 +0300

    Update index.py

--- index.py
+++ index.py
@@ -120,8 +120,9 @@ def check_payment_webhook():
 @app.route('/webhook2')
 def webhook2():
     #payment_info=session.get("payment_info", None)
-    payment_info=request.get_json()
+    #payment_info=request.get_json()
     #flash("payment_info")
+    payment_info = json.loads(request.data, strict=False) # strict = False allow for escaped char
     
     return render_template("webhook2.html", payment_info=payment_info)
 
